9e2deea36a902a9ec4891a062ad046d50dc26fea
Yu-Zhuang <106711129@gms.tcu.edu.tw>
2021-04-25 21:58:37 +0800

    debug: add payment frontend

--- front/static/javascript/payment.js
+++ front/static/javascript/payment.js
@@ -1,8 +1,8 @@
-function addPayment() {
+async function addPayment() {
     date = String(document.getElementById("date").value)
     _class = String(document.getElementById("class").value)
-    payment = String(document.getElementById("class").value)
-    remark = String(document.getElementById("class").value)
+    payment = String(document.getElementById("payment").value)
+    remark = String(document.getElementById("remark").value)
     if (date == "" || _class == "" || payment == "" || remark == "") {
         alert("尚未填寫完整")
         return
