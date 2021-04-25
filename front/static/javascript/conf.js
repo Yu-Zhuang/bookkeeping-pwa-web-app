9851340744b84db80ac7d3478a90d17aa8db395b
Yu-Zhuang <106711129@gms.tcu.edu.tw>
2021-04-25 16:20:34 +0800

    recover argu

--- front/static/javascript/conf.js
+++ front/static/javascript/conf.js
@@ -1,2 +1,2 @@
-//const hostUrl = "https://go-keep.herokuapp.com/"
- const hostUrl = "http://127.0.0.1:8080/"
+const hostUrl = "https://go-keep.herokuapp.com/"
+// const hostUrl = "http://127.0.0.1:8080/"
