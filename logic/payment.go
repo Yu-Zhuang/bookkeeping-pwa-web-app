734b028a17ed9c232155715f6f0863ddb649db4a
Yu-Zhuang <106711129@gms.tcu.edu.tw>
2021-04-25 22:17:33 +0800

    debug payment insert to db

--- logic/payment.go
+++ logic/payment.go
@@ -4,11 +4,15 @@ import (
 	"bookkeeping/dao"
 	"bookkeeping/model"
 	"fmt"
+	"strconv"
+	"time"
 )
 
 func CreatePayment(p model.PaymentRecord) bool {
 	sql_statement := "INSERT INTO paymentrecord (class, date, payment, remark, personID) VALUES ($1, $2, $3, $4, $5);"
-	_, err := dao.PostgresDB.Exec(sql_statement, p.Class, p.Date, p.Payment, p.Remark, p.PersonID)
+	payment, _ := strconv.Atoi(p.Payment)
+	date, _ := time.Parse("2006-01-02", p.Date)
+	_, err := dao.PostgresDB.Exec(sql_statement, p.Class, date, payment, p.Remark, p.PersonID)
 	if err != nil {
 		fmt.Println(err.Error())
 		return false
