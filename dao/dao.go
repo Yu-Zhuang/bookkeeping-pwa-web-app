71195967787f96d54434e71d7a5e6daad358b427
Yu-Zhuang <106711129@gms.tcu.edu.tw>
2021-04-25 23:41:35 +0800

    update

--- dao/dao.go
+++ dao/dao.go
@@ -25,19 +25,19 @@ func ConnectPostgresDB() bool {
 }
 
 func InitDB() bool {
-	if _, err := PostgresDB.Exec("DROP TABLE IF EXISTS person;"); err != nil {
-		log.Fatal(err)
-		return false
-	}
+	// if _, err := PostgresDB.Exec("DROP TABLE IF EXISTS person;"); err != nil {
+	// 	log.Fatal(err)
+	// 	return false
+	// }
 	if _, err := PostgresDB.Exec("CREATE TABLE IF NOT EXISTS person (id VARCHAR(20) PRIMARY KEY, name VARCHAR(20), password VARCHAR(50), email VARCHAR(60));"); err != nil {
 		log.Fatal(err)
 		return false
 	}
 
-	if _, err := PostgresDB.Exec("DROP TABLE IF EXISTS paymentrecord;"); err != nil {
-		log.Fatal(err)
-		return false
-	}
+	// if _, err := PostgresDB.Exec("DROP TABLE IF EXISTS paymentrecord;"); err != nil {
+	// 	log.Fatal(err)
+	// 	return false
+	// }
 	if _, err := PostgresDB.Exec("CREATE TABLE IF NOT EXISTS paymentrecord (id serial PRIMARY KEY, class VARCHAR(20), payment INTEGER, date DATE, remark VARCHAR(30), personID VARCHAR(20), CONSTRAINT fk_person FOREIGN KEY(personID) REFERENCES person(id));"); err != nil {
 		log.Fatal(err)
 		return false
