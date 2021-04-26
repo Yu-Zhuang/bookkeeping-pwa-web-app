68bc85cd2cd71b496af661d1570f878a07675524
Yu-Zhuang <106711129@gms.tcu.edu.tw>
2021-04-26 16:32:28 +0800

    add pie chart color

--- front/static/javascript/index.js
+++ front/static/javascript/index.js
@@ -53,7 +53,14 @@ function getPieChart(pieLabels, pieData) {
         datasets: [{
             label: '分佈圖',
             data: pieData,
-            backgroundColor: Object.values(Utils.CHART_COLORS),
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
             hoverOffset: 6
         }]
         };
