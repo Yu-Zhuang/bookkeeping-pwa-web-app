4edef3f3507cea968459e37668d4953370c4cf89
Yu-Zhuang <106711129@gms.tcu.edu.tw>
2021-04-26 10:26:38 +0800

    add list payment history

--- controller/crud.go
+++ controller/crud.go
@@ -2,8 +2,10 @@ package controller
 
 import (
 	"bookkeeping/config"
+	"bookkeeping/dao"
 	"bookkeeping/logic"
 	"bookkeeping/model"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -19,4 +21,29 @@ func AddPayment(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, nil)
+}
+
+func GetPaymentHistory(c *gin.Context) {
+	userID := c.MustGet(config.AuthMidUserNameKey).(string)
+
+	sql_statement := `SELECT class, payment, date, remark FROM paymentrecord WHERE personid=$1`
+	rows, err := dao.PostgresDB.Query(sql_statement, userID)
+	if err != nil {
+		fmt.Println(err.Error())
+		c.JSON(http.StatusBadRequest, nil)
+		return
+	}
+	retData := []model.PaymentRecord{}
+	var tmpData model.PaymentRecord
+
+	for rows.Next() {
+		if err := rows.Scan(&tmpData.Class, &tmpData.Payment, &tmpData.Date, &tmpData.Remark); err != nil {
+			fmt.Println(err.Error())
+			c.JSON(http.StatusBadRequest, nil)
+		}
+		retData = append(retData, tmpData)
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"data": retData,
+	})
 }
